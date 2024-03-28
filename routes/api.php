4f9d650149e907632674b42f40a08545517cf595
phanphongspvp <phanphongspvp@gmail.com>
2024-03-28 20:23:40 +0700

    feat: add authenticate sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post("/register", [AuthController::class, "register"]);
+Route::post("/login", [AuthController::class, "login"]);
+
+Route::group(["middleware" => ["auth:sanctum"]], function () {
+    Route::get("/products", [ProductController::class, "index"]);
+    Route::post("/logout", [AuthController::class, "logout"]);
+});
+
+Route::middleware('auth:sanctum')->get('/user', function () {
+    Route::post("/products", [ProductController::class, "store"]);
 });
